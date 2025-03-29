7118254cf0f1a6a38b275b4d11eb5ca57c6f7b5b
Rano <ranotoktorbaeva@gmail.com>
2025-03-29 10:42:36 +0600

    Rano/Strin1/ComboString

---
+++ src/main/java/rano/String_1/ComboString.java
@@ -0,0 +1,11 @@
+package rano.String_1;
+
+public class ComboString {
+        public String comboString(String a, String b) {
+        if (a.length() > b.length()) {
+            return b + a + b;
+        } else {
+            return (a + b + a);
+        }
+    }
+}
